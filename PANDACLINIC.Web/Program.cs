1f5a01402787dccd0f8527753820043b2d739d62
ahmed01114790265 <01114790265ahmed.com>
2026-05-11 14:10:46 +0300

    comment hts

--- PANDACLINIC.Web/Program.cs
+++ PANDACLINIC.Web/Program.cs
@@ -78,7 +78,7 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
+   // app.UseHsts();
 }
 
 using (var scope = app.Services.CreateScope())
